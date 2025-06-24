6ef4a248514ead23a9e09f58116db315af0e77ac
Hoang Tien Lang <langhoangtien@gmail.com>
2025-06-25 00:50:34 +0700

    update theme shine pro 2

--- src/layout/main-layout/footer.tsx
+++ src/layout/main-layout/footer.tsx
@@ -34,75 +34,95 @@ const MENU = [
 ];
 export default function Footer() {
   return (
-    <footer className="border-t bg-primary text-accent border-border  py-8 px-4 md:px-16">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Company Info */}
-        <div className="space-y-4 flex flex-col items-start justify-start">
-          <LogoWithLink />
+    <footer className="bg-primary text-accent border-border ">
+      <div className="bg-background text-primary">
+        {" "}
+        <svg
+          className="h-10 md:h-20"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 226.97"
+          width="100%"
+          preserveAspectRatio="none"
+        >
+          <path
+            fill="currentColor"
+            fill-opacity="1"
+            d="M0,81,21.8,70.3C43.6,60,87,38,131,27.7,174.5,17,218,17,262,49c43.5,32,87,96,131,106.7C436.4,166,480,124,524,97c43.3-27,87-37,131-26.7,43.2,10.7,87,42.7,130,64C829.1,156,873,166,916,161c44-5,88-27,131-58.7C1090.9,70,1135,28,1178,17c43.8-11,87,11,131,26.7,43.7,16.3,87,26.3,109,32l22,5.3V241H0Z"
+            transform="translate(0 -14.03)"
+          ></path>
+        </svg>
+      </div>
 
-          <p className="text-sm mt-2">Email: {CONTACT_EMAIL}</p>
-          {/* <div className="flex space-x-4 mt-4 text-xl">
+      <div className="py-8 px-4 md:px-16">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+          {/* Company Info */}
+          <div className="space-y-4 flex flex-col items-start justify-start">
+            <LogoWithLink />
+
+            <p className="text-sm mt-2">Email: {CONTACT_EMAIL}</p>
+            {/* <div className="flex space-x-4 mt-4 text-xl">
           <FaFacebookF className="cursor-pointer hover:text-blue-500" />
           <FaYoutube className="cursor-pointer hover:text-red-500" />
           <FaTiktok className="cursor-pointer hover:text-black" />
         </div> */}
-        </div>
-
-        {/* More Info */}
-        {MENU.map((item) => (
-          <div key={item.title}>
-            <h3 className="text-lg mb-4 font-semibold">{item.title}</h3>
-            <ul className="mt-2 space-y-4  text-base">
-              {item.items.map((subItem) => (
-                <li
-                  key={subItem.title}
-                  className="hover:underline cursor-pointer"
-                >
-                  <Link to={subItem.link} key={subItem.title}>
-                    {subItem.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
           </div>
-        ))}
 
-        {/* Policy */}
-      </div>
+          {/* More Info */}
+          {MENU.map((item) => (
+            <div key={item.title}>
+              <h3 className="text-lg mb-4 font-semibold">{item.title}</h3>
+              <ul className="mt-2 space-y-4  text-base">
+                {item.items.map((subItem) => (
+                  <li
+                    key={subItem.title}
+                    className="hover:underline cursor-pointer"
+                  >
+                    <Link to={subItem.link} key={subItem.title}>
+                      {subItem.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
 
-      {/* Bottom Section */}
-      <div className="mt-8 border-t pt-4 flex flex-col md:flex-row justify-between items-center text-sm">
-        <div className="flex items-center space-x-2">
-          <span className="text-red-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.34em"
-              height="1em"
-              viewBox="0 0 640 480"
-            >
-              <path fill="#bd3d44" d="M0 0h640v480H0" />
-              <path
-                stroke="#fff"
-                strokeWidth="37"
-                d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640"
-              />
-              <path fill="#192f5d" d="M0 0h364.8v258.5H0" />
-              <marker id="flagUs4x30" markerHeight="30" markerWidth="30">
-                <path fill="#fff" d="m14 0l9 27L0 10h28L5 27z" />
-              </marker>
-              <path
-                fill="none"
-                markerMid="url(#flagUs4x30)"
-                d="m0 0l16 11h61h61h61h61h60L47 37h61h61h60h61L16 63h61h61h61h61h60L47 89h61h61h60h61L16 115h61h61h61h61h60L47 141h61h61h60h61L16 166h61h61h61h61h60L47 192h61h61h60h61L16 218h61h61h61h61h60z"
-              />
-            </svg>
-          </span>
-          <span>English (EN) | USD</span>
+          {/* Policy */}
         </div>
 
-        <div className="flex space-x-2 mt-2 md:mt-0">
-          <p className="mt-2 md:mt-0 ">Powered by {COMPANY_NAME}</p>
-          <ListPaymentMethod />
+        {/* Bottom Section */}
+        <div className="mt-8 border-t pt-4 flex flex-col md:flex-row justify-between items-center text-sm">
+          <div className="flex items-center space-x-2">
+            <span className="text-red-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.34em"
+                height="1em"
+                viewBox="0 0 640 480"
+              >
+                <path fill="#bd3d44" d="M0 0h640v480H0" />
+                <path
+                  stroke="#fff"
+                  strokeWidth="37"
+                  d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640"
+                />
+                <path fill="#192f5d" d="M0 0h364.8v258.5H0" />
+                <marker id="flagUs4x30" markerHeight="30" markerWidth="30">
+                  <path fill="#fff" d="m14 0l9 27L0 10h28L5 27z" />
+                </marker>
+                <path
+                  fill="none"
+                  markerMid="url(#flagUs4x30)"
+                  d="m0 0l16 11h61h61h61h61h60L47 37h61h61h60h61L16 63h61h61h61h61h60L47 89h61h61h60h61L16 115h61h61h61h61h60L47 141h61h61h60h61L16 166h61h61h61h61h60L47 192h61h61h60h61L16 218h61h61h61h61h60z"
+                />
+              </svg>
+            </span>
+            <span>English (EN) | USD</span>
+          </div>
+
+          <div className="flex space-x-2 mt-2 md:mt-0">
+            <p className="mt-2 md:mt-0 ">Powered by {COMPANY_NAME}</p>
+            <ListPaymentMethod />
+          </div>
         </div>
       </div>
     </footer>
